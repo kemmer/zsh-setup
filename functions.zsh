75bb7b106d7f77468005a336e02fabb917dd94e9
Cristiano Kemmer <criskemmer@gmail.com>
2019-05-02 00:11:11 +0200

    Adding loglist and file counting functions

--- functions.zsh
+++ functions.zsh
@@ -10,12 +10,41 @@ function logstore()
     echo "$1" >> $target_path
 }
 
+function loglist()
+{
+    target_path="$SHSTORAGE_PATH/all.log"
+    if [[ $1 ]]; then
+        target_path="$SHSTORAGE_PATH/$1"
+    fi
+
+    cat $target_path
+}
+
 # File search functions
 function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
 
 # Create a folder and move into it in one command
 function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Find out number of lines per file and order them
+# References:
+# https://stackoverflow.com/questions/1358540/how-to-count-all-the-lines-of-code-in-a-directory-recursively
+# https://unix.stackexchange.com/questions/260630/how-do-you-list-number-of-lines-of-every-file-in-a-directory-in-human-readable-f
+function fcount()
+{
+    if [[ ! $1 ]]; then
+        echo "fcount: Please provide the folder path"
+        return 1
+    fi
+
+    extension="*"
+    if [[ $2 ]]; then
+        extension="$2"
+    fi
+
+    find "$1" -name "*.${extension}" | xargs wc -l | sort
+}
 
 # Dictionaries
 function dict()
@@ -43,7 +72,7 @@ function thesau()
 function gpdict()
 {
     if [[ ! $1 ]]; then
-        echo "gdict: Please provide at least one german or portuguese"
+        echo "gdict: Please provide at least one german or portuguese word"
         return 1
     fi
 
