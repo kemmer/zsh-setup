fd97f6f96dd959349f369f8fa1ca2985db3c83c7
Cristiano Kemmer <criskemmer@gmail.com>
2019-08-25 12:52:26 +0200

    Updating env vars

--- env.zsh
+++ env.zsh
@@ -1,17 +1,44 @@
-# ssh
-export SSH_KEY_PATH="~/.ssh/rsa_id"
+unsetopt share_history
+export HISTSIZE=500000
+export SAVEHIST=100000
+export HISTCONTROL=erasedups
+export HISTIGNORE="&:ls:ll:la:l.:pwd:exit:clear"
+
+export EDITOR='vim'
+export FILES='/Volumes/Personal'
+
+# ssh key
+export SSH_KEY_PATH="$HOME/.ssh/rsa_id"
 
 # User/local binaries in the PATH
-export PATH=~/.local/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+export PATH="/usr/local/opt/curl/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 
-# Disables command history sharing between terminals
-unsetopt share_history
+# Compiler flags
+#export LDFLAGS="-L/usr/local/opt/curl/lib"
+#export CPPFLAGS="-I/usr/local/opt/curl/include"
 
-HISTSIZE=500000
-SAVEHIST=100000
+# golang
+export GOPATH="$HOME/Projects/golang"
 
-export EDITOR=vim
+# Extending oh-my-zsh default theme
+PROMPT='${ret_status} %{$fg[cyan]%}%~%{$reset_color%} $(git_prompt_info)'
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[yellow]%}"
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(ssh dir vcs newline status)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
-POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+# Hiding kube information by default and changing layout
+KUBE_PS1_SYMBOL_ENABLE=false
+RPS1='$(kube_ps1) $EPS1'
+
+# Fixing locale
+export LC_ALL='en_US.UTF-8'
+
+# phpbrew
+# source $HOME/.phpbrew/bashrc
+
+# Completions
+source <(kubectl completion zsh)
+
