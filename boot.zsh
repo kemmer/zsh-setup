e98e930909896bc74f05a297270f59d6a5db1fce
Cristiano Kemmer <criskemmer@gmail.com>
2019-08-25 12:52:59 +0200

    Changing order of inclusion of sensitive in boot.zsh

--- boot.zsh
+++ boot.zsh
@@ -1,6 +1,5 @@
 # -- Environment variables
 source $SHCONFIG_PATH/env.zsh
-source $SHCONFIG_PATH/sensitive.zsh
 
 # -- Aliases
 source $SHCONFIG_PATH/alias.zsh
@@ -8,5 +7,9 @@ source $SHCONFIG_PATH/alias.zsh
 # -- Functions and helpers
 source $SHCONFIG_PATH/functions.zsh
 
+# -- Sensitive functions, env vars, alises, etc
+[ -f $SHCONFIG_PATH/sensitive.zsh ] && source $SHCONFIG_PATH/sensitive.zsh
+
 # -- Exec at startup
-(neofetch && echo "\n\n" && quote)
+#(neofetch && echo "\n\n" && quote)
+kubeoff
